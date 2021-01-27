49082e6cf3db9af06422f3b23aa871f1522079d2
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-01-27 15:44:21 +0100

    await Firebase.initializeApp();

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce_mini_app/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
   // main
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
